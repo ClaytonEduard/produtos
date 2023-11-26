ea61a5f336002bba673cc823495df77680cd3bf7
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 10:26:48 -0300

    Configurando a rota get

--- src/main/java/br/com/produtos/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/produtos/api/produtos/controle/ProdutoControle.java
@@ -1,5 +1,12 @@
 package br.com.produtos.api.produtos.controle;
 
-public class ProdutoControle {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController // responsavel por criar as rotas
+public class ProdutoControle {
+    @GetMapping("/")
+    public String rota() {
+        return "API de produtos funcionando!";
+    }
 }
