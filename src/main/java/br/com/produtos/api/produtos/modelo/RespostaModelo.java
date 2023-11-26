c00e228eb9022b60f6c79e0a802f167226db19ce
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 10:29:31 -0300

    Implementando a classe de resposta caso erro

--- src/main/java/br/com/produtos/api/produtos/modelo/RespostaModelo.java
+++ src/main/java/br/com/produtos/api/produtos/modelo/RespostaModelo.java
@@ -1,5 +1,13 @@
 package br.com.produtos.api.produtos.modelo;
 
-public class RespostaModelo {
+import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class RespostaModelo {
+    private String mensagem;
 }
