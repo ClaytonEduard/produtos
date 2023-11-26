b81a8955650ad261e945d6da9e627269fb56f841
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 10:32:46 -0300

    Service para llistar todos os produtos

--- src/main/java/br/com/produtos/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/produtos/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,18 @@
 package br.com.produtos.api.produtos.servico;
 
-public class ProdutoServico {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import br.com.produtos.api.produtos.modelo.ProdutoModelo;
+import br.com.produtos.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
+public class ProdutoServico {
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    // metodo para listar todos os produtos
+    public Iterable<ProdutoModelo> listar() {
+        return pr.findAll();
+    }
 }
