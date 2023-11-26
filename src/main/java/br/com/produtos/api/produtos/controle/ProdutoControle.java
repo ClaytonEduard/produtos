1f4af46886d13ba7087a66216a2cf329cfd7157f
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 10:36:30 -0300

    Criando Rota listar produtos

--- src/main/java/br/com/produtos/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/produtos/api/produtos/controle/ProdutoControle.java
@@ -1,10 +1,23 @@
 package br.com.produtos.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.produtos.api.produtos.modelo.ProdutoModelo;
+import br.com.produtos.api.produtos.servico.ProdutoServico;
+
 @RestController // responsavel por criar as rotas
 public class ProdutoControle {
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
+    }
+
     @GetMapping("/")
     public String rota() {
         return "API de produtos funcionando!";
