2c4793268c5ec1ae5f86310cb2513224778c57dc
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 11:58:40 -0300

    Rota remover produto criada

--- src/main/java/br/com/produtos/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/produtos/api/produtos/controle/ProdutoControle.java
@@ -2,13 +2,16 @@ package br.com.produtos.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.produtos.api.produtos.modelo.ProdutoModelo;
+import br.com.produtos.api.produtos.modelo.RespostaModelo;
 import br.com.produtos.api.produtos.servico.ProdutoServico;
 
 @RestController // responsavel por criar as rotas
@@ -16,6 +19,11 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico ps;
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+        return ps.remover(codigo);
+    }
 
     @PutMapping("alterar")
     public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
